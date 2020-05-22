27b3633991a36c1ed8faaa38b8588be89ce61f03
Shardul Deodhar <deodharshardul@gmail.com>
2020-05-22 20:41:47 +0530

    run with dataset functionality complete

--- core/start_clustering.py
+++ core/start_clustering.py
@@ -6,55 +6,62 @@ from .triangle_inequality import *
 from .faster_triangle_inequality import *
 from .datasetloader import datasetloader
 
-def start_clustering(numClusters,kmeans_threshold,centroids_to_remember):
+def start_clustering(numClusters,kmeans_threshold,centroids_to_remember,algorithm):
 	pointList=datasetloader("birch")
 	print("here")
 	# numClusters=10 #take input
 	numClusters=int(numClusters)
 	kmeans_threshold=int(kmeans_threshold)
 	centroids_to_remember=int(centroids_to_remember)
-	Time=[]
+	# Time=[]
 
-	net_start=time.time()
+	# net_start=time.time()
+	if algorithm=="kmeans":
+		start = time.time()
+		config1= Kmeans(numClusters,pointList,kmeans_threshold)
+		return("Time taken for Kmeans: "+str(time.time() - start))
 
-	start = time.time()
-	config1= Kmeans(numClusters,pointList,kmeans_threshold)
-	Time.append("Kmeans Time taken:"+str(time.time() - start))
+	elif algorithm=="faster_kmeans":
+		start = time.time()
+		config2= Faster_Kmeans(numClusters,pointList,kmeans_threshold,centroids_to_remember)
+		return("Time taken for Faster Kmeans: "+str(time.time() - start))
 
-	start = time.time()
-	config2= Faster_Kmeans(numClusters,pointList,kmeans_threshold,centroids_to_remember)
-	Time.append("Faster Kmeans Time taken:"+str(time.time() - start))
+	elif algorithm=="enhanced_kmeans":
+		start = time.time()
+		config3= Enhanced_Kmeans(numClusters,pointList,kmeans_threshold)
+		return("Time taken for Enhanced Kmeans: "+str(time.time() - start))
 
-	start = time.time()
-	config3= Enhanced_Kmeans(numClusters,pointList,kmeans_threshold)
-	Time.append("Enhanced Kmeans Time taken:"+str(time.time() - start))
+	elif algorithm=="faster_enhanced_kmeans":
+		start = time.time()
+		config4= Faster_Enhanced_Kmeans(numClusters,pointList,kmeans_threshold,centroids_to_remember)
+		return("Time taken for Faster Enhanced Kmeans: "+str(time.time() - start))
 
-	start = time.time()
-	config4= Faster_Enhanced_Kmeans(numClusters,pointList,kmeans_threshold,centroids_to_remember)
-	Time.append("Faster Enhanced Kmeans Time taken:"+str(time.time() - start))
+	
+	elif algorithm=="triangle_inequality":
+		start = time.time()
+		config5= Triangle_Inequality(numClusters,pointList,kmeans_threshold)
+		Time="Time taken for Triangle Inequality: "+str(time.time() - start)
+		cList=[]
+		for centroid in config5.centroidList:
+			point = centroid.centerPos[0]
+			cList.append(point)
+		start = time.time()
+		config6= Triangle_Inequality(numClusters,pointList,kmeans_threshold,cList)
+		Time+=" seconds, Time taken for Triangle Inequality with initial centorids: "+str(time.time() - start)
+		return Time
 
-	start = time.time()
-	config5= Triangle_Inequality(numClusters,pointList,kmeans_threshold)
-	Time.append("Triangle_Inequality Time taken:"+str(time.time() - start))
-	cList=[]
-	for centroid in config5.centroidList:
-		point = centroid.centerPos[0]
-		cList.append(point)
-	start = time.time()
-	config6= Triangle_Inequality(numClusters,pointList,kmeans_threshold,cList)
-	Time.append("Triangle_Inequality Part 2 Time taken:"+str(time.time() - start))
-
-	start = time.time()
-	config7= Faster_Triangle_Inequality(numClusters,pointList,kmeans_threshold,centroids_to_remember)
-	Time.append("Faster_Triangle_Inequality Time taken:"+str(time.time() - start))
-	cList=[]
-	for centroid in config5.centroidList:
-		point = centroid.centerPos[0]
-		cList.append(point)
-	start = time.time()
-	config8= Faster_Triangle_Inequality(numClusters,pointList,kmeans_threshold,centroids_to_remember,cList)
-	Time.append("Faster_Triangle_Inequality Part 2 Time taken:"+str(time.time() - start))
-	return Time
+	elif algorithm=="faster_triangle_inequality":
+		start = time.time()
+		config7= Faster_Triangle_Inequality(numClusters,pointList,kmeans_threshold,centroids_to_remember)
+		Time="Time taken for Faster Triangle Inequality: "+str(time.time() - start)
+		cList=[]
+		for centroid in config7.centroidList:
+			point = centroid.centerPos[0]
+			cList.append(point)
+		start = time.time()
+		config8= Faster_Triangle_Inequality(numClusters,pointList,kmeans_threshold,centroids_to_remember,cList)
+		Time+=" seconds, Time taken for Faster Triangle Inequality with initial centorids: "+str(time.time() - start)
+		return Time
 
 
 
--- core/views.py
+++ core/views.py
@@ -7,17 +7,19 @@ from django.http import HttpResponse
 
 def run_dataset(request):
 	if request.method =='POST':
+		algorithm=request.POST.get('algorithm')
 		form= run_dataset_form(request.POST)
 		if form.is_valid():
 			number_of_clusters=form.cleaned_data['number_of_clusters']
 			kmeans_threshold=form.cleaned_data['kmeans_threshold']
 			centroids_to_remember=form.cleaned_data['centroids_to_remember']
-			time=start_clustering(number_of_clusters,kmeans_threshold,centroids_to_remember)
+			print(algorithm)
+			Time=start_clustering(number_of_clusters,kmeans_threshold,centroids_to_remember,algorithm)
 			print(Time)
-			if time is None:
+			if Time is None:
 				messages.success(request, f' Time exceeded, please enter reasonable parameters!')
 			else:
-				messages.success(request, f' Algorithm executed successfully!')
+				messages.success(request, f' %s seconds' %Time)
 	else:
 		form= run_dataset_form()
 	return render(request,'core/run_dataset.html',{'form':form})
